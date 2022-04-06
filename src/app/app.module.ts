45095cac68eaa3a8b09e9928c5481d87bba32479
Bogdannb34 <bogdan22bogdan@gmail.com>
2022-04-06 14:56:46 +0300

    components-created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryComponent } from './components/country/country.component';
+import { PortComponent } from './components/port/port.component';
+import { ShipComponent } from './components/ship/ship.component';
+import { VoyageComponent } from './components/voyage/voyage.component';
+import { ShowCountryComponent } from './components/country/show-country/show-country.component';
+import { AddEditCountryComponent } from './components/country/add-edit-country/add-edit-country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryComponent,
+    PortComponent,
+    ShipComponent,
+    VoyageComponent,
+    ShowCountryComponent,
+    AddEditCountryComponent
   ],
   imports: [
     BrowserModule,
